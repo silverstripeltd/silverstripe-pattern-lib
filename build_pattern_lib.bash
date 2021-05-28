4ac2c384e9810aea8bed251b7d6a85ee26363ca6
Maxime Rainville <maxime@silverstripe.com>
2021-05-28 15:50:45 +1200

    Add a build srcipt to generate pattern lib

---
+++ build_pattern_lib.bash
@@ -0,0 +1,8 @@
+git clone git@github.com:silverstripe/silverstripe-admin.git
+cd silverstripe-admin
+export COMPOSER_ROOT_VERSION=1.x-dev
+composer require silverstripe/installer:4.x-dev --prefer-source
+yarn install && yarn static-pattern-lib
+mv storybook-static ../
+cd ..
+rm -rf silverstripe-admin
