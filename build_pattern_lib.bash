958a0802abc270597925b700dc53bc8c5427b4e4
Maxime Rainville <maxime@silverstripe.com>
2021-05-28 16:03:42 +1200

    Clone admin with HTTPS Url

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -1,4 +1,4 @@
-git clone git@github.com:silverstripe/silverstripe-admin.git
+git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
 composer require silverstripe/installer:4.x-dev --prefer-source
