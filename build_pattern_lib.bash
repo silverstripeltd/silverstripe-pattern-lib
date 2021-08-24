95b742ce0c37ba958f867eb6a4e45a85c4c464ec
Maxime Rainville <maxime@silverstripe.com>
2021-08-24 12:17:18 +1200

    DBG Try to figure out why build is failing

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -11,7 +11,10 @@ git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
 ../composer.phar require silverstripe/installer:4.x-dev --prefer-source
-yarn install && yarn static-pattern-lib
+yarn install
+ls silverstripe-admin/.storybook
+cat silverstripe-admin/.storybook/.stories.js
+yarn static-pattern-lib
 mv storybook-static ../
 cd ..
 #rm -rf silverstripe-admin
