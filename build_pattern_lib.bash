8734f678f381b305a26ff44788bf538f235b2872
Maxime Rainville <maxime@silverstripe.com>
2021-08-24 17:11:31 +1200

    Trying building only for admin

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -9,7 +9,7 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
-../composer.phar require silverstripe/installer:4.x-dev --prefer-source
+#../composer.phar require silverstripe/installer:4.x-dev --prefer-source
 yarn cache clean
 yarn install
 yarn static-pattern-lib
