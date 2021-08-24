f2a29270b49b2582d6c238ceee8350493408a412
Maxime Rainville <maxime@silverstripe.com>
2021-08-24 12:49:52 +1200

    remove debug statement

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -6,16 +6,11 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
-yarn cache clean
 git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
 ../composer.phar require silverstripe/installer:4.x-dev --prefer-source
 yarn install
-pwd
-ls -la
-ls -la .storybook
-cat .storybook/.stories.js
 yarn static-pattern-lib
 mv storybook-static ../
 cd ..
