ef6ac90658367282985c8a3e3521b62b565aea32
Maxime Rainville <maxime@silverstripe.com>
2021-08-24 13:14:03 +1200

    clear yarn cache

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -10,6 +10,7 @@ git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
 ../composer.phar require silverstripe/installer:4.x-dev --prefer-source
+yarn cache clean
 yarn install
 yarn static-pattern-lib
 mv storybook-static ../
