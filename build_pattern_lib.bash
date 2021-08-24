a0e1491255d9a6aad1a5569455630d75178a523b
Maxime Rainville <maxime@silverstripe.com>
2021-08-24 17:16:50 +1200

    Manaually checkout elemental and asset-admin

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -9,7 +9,8 @@ trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 git clone https://github.com/silverstripe/silverstripe-admin.git
 cd silverstripe-admin
 export COMPOSER_ROOT_VERSION=1.x-dev
-#../composer.phar require silverstripe/installer:4.x-dev --prefer-source
+../composer.phar require silverstripe/asset-admin:4.x-dev --prefer-source
+../composer.phar require dnadesign/silverstripe-elemental:4.x-dev --prefer-source
 yarn cache clean
 yarn install
 yarn static-pattern-lib
