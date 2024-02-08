26015653c5df483617af31d4f68b0cb5c01a1325
Guy Sartorelli <36352093+GuySartorelli@users.noreply.github.com>
2024-02-08 16:00:45 +1300

    ENH Add linkfield to pattern library (#4)

--- build_pattern_lib.bash
+++ build_pattern_lib.bash
@@ -13,9 +13,11 @@ export COMPOSER_ROOT_VERSION=2.x-dev
 composer config allow-plugins.composer/installers true
 composer config allow-plugins.silverstripe/recipe-plugin true
 composer config allow-plugins.silverstripe/vendor-plugin true
+composer config allow-plugins.phpstan/extension-installer true
 ../composer.phar require silverstripe/asset-admin:2.x-dev --prefer-source --no-install
 ../composer.phar require silverstripe/session-manager:2.x-dev --prefer-source --no-install
 ../composer.phar require silverstripe/tagfield:3.x-dev --prefer-source --no-install
+../composer.phar require silverstripe/linkfield:4.x-dev --prefer-source --no-install
 ../composer.phar require dnadesign/silverstripe-elemental:5.x-dev --prefer-source
 yarn cache clean
 yarn install
